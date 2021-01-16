fe2a667a011ffac703e18d1906ab9ffcff237c7a
ajcudd <ajcuddeback@gmail.com>
2021-01-16 10:25:41 -0500

    added methods for candidates table in node

--- server.js
+++ server.js
@@ -23,6 +23,38 @@ app.get('/', (req, res) => {
     });
 });
 
+// db.all(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+// });
+
+// Get a single candidate
+// db.get(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(row);
+// });
+
+// Delete a candidate
+// db.run(`DELETE FROM candidates WHERE id = ?`, 1, function (err, result) {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// })
+
+// Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+                VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+// ES5 function, not arrow function to use this
+db.run(sql, params, function (err, result) {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result, this.lastID);
+});
+
 // Default response for any other request(Not Fount) - Catch all
 app.use((req, res) => {
     res.status(404).end();
